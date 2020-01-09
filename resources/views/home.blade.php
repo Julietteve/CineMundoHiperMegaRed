2a708d4188684d5e40ac75d11689eb26a0c7c616
Julieta E. Garcia <52335847+Julietteve@users.noreply.github.com>
2020-01-09 12:47:59 -0300

    Modelos y controladores principales

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,14 +14,6 @@
       <img src="..." class="d-block w-100" alt="...">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
 
 @endsection
